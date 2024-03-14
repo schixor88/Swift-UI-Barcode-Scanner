f74c5a72458ab5e78274e1f9288707b317ccff22
Kushagra <kushagra.acharya@outlook.com>
2024-03-14 12:11:23 +0545

    Initial Commit

---
+++ BarScanner/BarScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarScannerApp.swift
+//  BarScanner
+//
+//  Created by Kushagra Acharya on 14/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct BarScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
